f419c8f6c04ac58bd072ca4154e6a130b47c8b4c
Andreas Sander <mail@andi1984.de>
2020-04-27 23:13:35 +0200

    chore(tests) - Adding tests for URL utils.

---
+++ utils/__tests__/url.test.ts
@@ -0,0 +1,37 @@
+const { isAbsoluteURL, generateURL } = require('../url');
+
+describe('isAbsoluteURL', () => {
+  test('it fails if url is not given', () => {
+    expect(() => isAbsoluteURL()).toThrow();
+  });
+
+  test('it returns true if url is absolute', () => {
+    expect(isAbsoluteURL('https://www.google.com')).toBe(true);
+  });
+
+  test('it returns false if url is relative', () => {
+    expect(isAbsoluteURL('../foo')).toBe(false);
+  });
+});
+
+
+describe('generateURL', () => {
+    test('it fails if url is not given or not valid', () => {
+        expect(() => generateURL()).toThrow();
+    })
+
+    test('pathname', () => {
+        expect(generateURL('http://www.google.de').pathname).toBe('/');
+        expect(generateURL('http://www.google.de/foo').pathname).toBe('/foo');
+    })
+
+    test('href', () => {
+        expect(generateURL('http://www.google.de').href).toBe(
+          'http://www.google.de/'
+        );
+
+        expect(generateURL('http://www.google.de/foo?myquery=2').href).toBe(
+          'http://www.google.de/foo?myquery=2'
+        );
+    })
+})
--- utils/url.ts
+++ utils/url.ts
@@ -1,16 +1,27 @@
-import { URL } from "url";
+import { URL } from 'url';
 
 // https://stackoverflow.com/a/41975448/778340
-export {};
-const isAbsoluteURL = (url: string) => {
-  return url.indexOf("://") > 0 || url.indexOf("//") === 0;
+export { };
+  
+/**
+ * Returns whether URL is absolute or not.
+ * @param {string} url 
+ * @returns {boolean}
+ */
+const isAbsoluteURL = (url: string): Boolean => {
+  return url.indexOf('://') > 0 || url.indexOf('//') === 0;
 };
 
+/**
+ * Generates a new URL for the given link.
+ * @param {string} link
+ * @returns {URL}
+ */
 const generateURL = (link: string): URL => {
   return new URL(
     Boolean(process.env.PRETTY_URLS) ||
     isAbsoluteURL(link) ||
-    link.indexOf(".html") !== -1
+    link.indexOf('.html') !== -1
       ? link
       : `${link}.html`,
     process.env.URL
@@ -20,5 +31,5 @@ const generateURL = (link: string): URL => {
 module.exports = {
   // https://stackoverflow.com/a/38979205/778340
   isAbsoluteURL,
-  generateURL
+  generateURL,
 };
